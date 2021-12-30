d3d9371876eaf28605721d6997d0ab7e2ab1366a
feng_lin <feng_lin@daydao.com>
2021-12-30 17:46:41 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -16,10 +16,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f https://github.com/Zack921/Zack921.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# 发布到 https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/jserlin/ex-element.git master:gh-pages
 
 cd -
